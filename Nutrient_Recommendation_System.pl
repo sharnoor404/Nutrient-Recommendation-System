3dc0b1caeaed3e706da5128ec740e7cb7b4b11a9
sharnoor404 <sharnoor404@gmail.com>
2020-04-25 20:29:01 +0530

    Initial Commit

---
+++ Nutrient_Recommendation_System.pl
@@ -0,0 +1,50 @@
+food('COCONUT WATER(1 cup)',18,0.22,4.24,0,7,0.03,'BEV','V').
+food('CRANBERRY JUICE(1 cup)',46,0.39,12.2,0.1,8,0.25,'BEV','V').
+food('CRANBERRY-APPLE JUICE(1 cup)',19,0.1,4.7,0.1,10,0.06,'BEV','V').
+food('HOT COCOA MILK(1 serving)',400,3,75,4,100,1.8,'BEV','V').
+food('LEMON ICED TEA(1 fl oz)',35,0,8.81,0,1,0,'BEV','V').
+food('MIXED VEG & FRUIT JUICE(1 fl oz)',29,0.04,7.47,0,3,0.04,'BEV','V').
+food('PEACH MANGO SMOOTHIE(1 serving)',37,1.22,7.76,0,41,0.15,'BEV','V').
+food('TOMATO & VEG JUICE(1 cup)',22,0.6,4.59,0.8,11,0.42,'BEV','V').
+food('VANILLA SOYMILK(1 cup)',43,2.6,4.92,0.2,123,0.42,'BEV','V').
+food('LOW FAT MILK(1 cup)',42,3.37,4.99,0,125,0.03,'BEV','V').
+food('BUTTER MILK(1 cup)',40,3.31,4.79,0,116,0.05,'BEV','V').
+food('CHOCOLATE MILK(1 cup)',77,3.52,10.74,1,114,0.42,'BEV','V').
+food('BUFFALO MILK(1 cup)',97,3.75,5.18,0,169,0.12,'BEV','V').
+food('SHEEP MILK(1 cup)',108,5.98,5.36,0,193,0.1,'BEV','V').
+food('THICK CHOCOLATE MILK SHAKE(1 fl oz)',119,3.05,21.15,0.3,132,0.31,'BEV','V').
+food('THICK VANILLA MILK SHAKE(1 fl oz)',112,3.86,17.75,0,146,0.1,'BEV','V').
+food('MANGO SMOOTHIE(8 fl oz)',63,0.42,15,0,8,0.15,'BEV','V').
+food('BERRY SMOOTHIE(1 cup)',46,0.63,10.9,0,10,0.21,'BEV','V').
+food('STRAWBERRY BANANA SMOOTHIE(1 cup)',55,0.31,12.76,1,9,0.14,'BEV','V').
+food('ORANGE PINEAPPLE JUICE(8 fl oz)',51,0.41,12.2,0.2,8,0.18,'BEV','V').
+food('GRAPEFRUIT JUICE(1 cup)',39,0.5,9.2,0,9,0.2,'BEV','V').
+food('TOMATO JUICE(1 cup)',17,0.85,3.53,0.4,10,0.39,'BEV','V').
+food('CANED CARROT JUICE (1 cup)',40,0.95,9.28,0.8,24,0.46,'BEV','V').
+food('ORANGE JUICE(1 cup)',54,0.2,13.41,0.2,2,0.11,'BEV','V').
+food('GUAVA PASSION FRUIT JUICE(1 serving, 8 oz)',33,0,7.82,0,0,0,'BEV','V').
+food('MANGO PEACH JUICE(1 serving, 8 oz)',33,0,8.23,0,0,0,'BEV','V').
+food('ORANGE PINEAPPLE JUICE(1 serving, 8 oz)',29,0,7.41,0,0,0,'BEV','V').
+food('STRAWBERRY BANANA JUICE(1 serving, 8 oz)',29,0,7.41,0,0,0,'BEV','V').
+food('STRAWBERRY KIWI JUICE(1 serving, 8 oz)',29,0,7.41,0,0,0,'BEV','V').
+food('Tropical Juice(1 serving, 8 oz)',29,0,7.41,0,0,0,'BEV','V').
+food('CHAPATI (1 piece)',297,11.25,46.36,4.9,93,3.01,'BREAD','V').
+food('FRIED RICE(1 cup)',174,4.05,32.79,1.1,12,0.66,'BREAD','V').
+food('NAAN (1 piece)',291,9.62,50.43,2.2,84,3.25,'BREAD','V').
+food('RICE COOKED(1 cup)',123,2.91,26.05,0.9,19,0.24,'BREAD','V').
+food('WILD RICE COOKED(1 cup)',101,3.99,21.34,1.8,3,0.6,'BREAD','V').
+food('SPANISH RICE(1 cup)',185,3.28,31.16,1.2,20,1.15,'BREAD','V').
+food('APPLE CROISSANTS(1 oz)',254,7.4,37.1,2.5,30,1.1,'BSIDE','V').
+food('BLUEBERRY MUFFINS(1 oz)',375,4.49,53,1.1,44,1.3,'BSIDE','V').
+food('BLUEBERRY PANCAKES(1 oz)',222,6.1,29,0,206,1.72,'BSIDE','V').
+food('BREAD AND EGGS(1 oz)',287,9.5,47.8,2.3,93,3.04,'BSIDE','N').
+food('BREAD TOAST(1 oz)',264,9.7,47.3,5.3,75,3.92,'BSIDE','V').
+food('BUTTER CROISSANTS(1 oz)',406,8.2,45.8,2.6,37,2.03,'BSIDE','V').
+food('CHEESE CROISSANTS(1 oz)',414,9.2,47,2.6,53,2.15,'BSIDE','V').
+food('CHEESE DANISH PASTRY(1 oz)',374,8,37.2,1,35,1.8,'BSIDE','V').
+food('CINNAMON-RAISIN BAGELS(1 mini bagel,(2-1/2" dia))',274,9.8,55.2,2.3,19,3.8,'BSIDE','V').
+food('CORN FLAKE AND FRUIT BAR(1 oz)',377,4.4,72.9,2.1,41,4.86,'BSIDE','V').
+food('CRISPED RICE BAR WITH ALMONDS(1 bar)',458,7,64.6,3.6,74,6.35,'BSIDE','V').
+food('EGG BAGELS(1 oz)',278,10.6,53,2.3,13,3.98,'BSIDE','N').
+food('FRENCH TOAST(1 oz)',213,7.4,32.1,1.1,107,2.21,'BSIDE','N').
+food('FRIED FRUIT PIE(1 oz)',316,3,42.6,2.6,22,1.22,'BSIDE','V').
