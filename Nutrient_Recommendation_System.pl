39043e6e6b974fe7040265946d51b76cbf9b0feb
sharnoor404 <sharnoor404@gmail.com>
2020-04-25 20:48:51 +0530

    Food Database

--- Nutrient_Recommendation_System.pl
+++ Nutrient_Recommendation_System.pl
@@ -48,3 +48,76 @@ food('CRISPED RICE BAR WITH ALMONDS(1 bar)',458,7,64.6,3.6,74,6.35,'BSIDE','V').
 food('EGG BAGELS(1 oz)',278,10.6,53,2.3,13,3.98,'BSIDE','N').
 food('FRENCH TOAST(1 oz)',213,7.4,32.1,1.1,107,2.21,'BSIDE','N').
 food('FRIED FRUIT PIE(1 oz)',316,3,42.6,2.6,22,1.22,'BSIDE','V').
+food('GLAZED DOUGHNUTS(1 oz)',421,6.14,47.93,2.1,101,2.38,'BSIDE','V').
+food('GRAHAM CRACKERS(1 serving)',386,5.71,77.96,5.7,429,3.09,'BSIDE','V').
+food('MUFFINS(1 oz)',223,8.7,44.8,4.6,178,2.87,'BSIDE','V').
+food('OAT BRAN BAGELS(1 mini bagel,(2-1/2" dia))',255,10.7,53.3,3.6,12,3.08,'BSIDE','V').
+food('OATMEAL COOKIES(3 cookies)',451,6.7,66.6,8.5,0,2.8,'BSIDE','V').
+food('PANCAKES(1 oz)',208,8.5,29.4,2.8,250,3.11,'BSIDE','V').
+food('PEANUT BAR(1 oz)',522,15.5,47.4,4.1,78,0.97,'BSIDE','V').
+food('TOASTED BREAD AND EGGS(1 oz)',315,10.5,52.6,2.5,102,3.34,'BSIDE','N').
+food('TOFU YOGURT(1 cup)',94,3.5,15.96,0.2,118,1.06,'BSIDE','V').
+food('WAFFLES(1 serving, 2 waffles)',257,7.14,49.16,4.3,143,6.43,'BSIDE','V').
+food('PLAIN YOGURT(1 container,(6 oz))',61,3.47,4.66,0,121,0.05,'BEV','V').
+food('VANILLA FLAVOURED YOGURT(1 container,(6 oz))',85,4.93,13.8,0,171,0.07,'BEV','V').
+food('FRUIT YOGURT(1 container,(6 oz))',99,3.98,18.64,0,138,0.06,'BEV','V').
+food('FRIED EGG(1 large)',196,13.61,0.83,0,62,1.89,'BSIDE','N').
+food('HARDBOILED EGG(1 cup, chopped)',155,12.58,1.12,0,50,1.19,'BSIDE','N').
+food('OMLET(1 large)',154,10.57,0.64,0,48,1.48,'BSIDE','N').
+food('POACHED EGGS(1 large)',143,12.51,0.71,0,56,1.75,'BSIDE','N').
+food('SCRAMBLED EGGS(1 large)',149,9.99,1.61,0,66,1.31,'BSIDE','N').
+food('VANILLA FLAVOURED GREEK YOGURT(5.3 oz)',85,8.12,12.72,0.5,89,0.04,'BEV','V').
+food('STRAWBERRY FLAVOURED GREEK YOGURT(5.3 oz)',84,8.03,12.53,0.4,94,0.06,'BEV','V').
+food('ALL-BRAN CEREAL(.5 cup)',260,13.14,74.24,29.3,389,17.6,'BSIDE','V').
+food('COCOA KRISPIES CEREAL(.75 cup)',389,4.6,87.9,1.4,127,14.5,'BSIDE','V').
+food('CORN FLAKES(1 cup)',357,7.5,84.1,3.3,5,28.9,'BSIDE','V').
+food('OAT BRAN CEREALS(.75 cup)',395,9.2,70.3,12.7,59,3.7,'BSIDE','V').
+food('FROOT LOOPS CEREALS(1 cup)',375,5.3,88,9.3,10,15.5,'BSIDE','V').
+food('HONEYCOMB CEREALS(1.5 cup)',396,6,87.1,3.1,11,8.4,'BSIDE','V').
+food('OATS CEREALS(.75 cup)',374,9.98,77.74,6.6,351,29.33,'BSIDE','V').
+food('CINNAMON DANISH PASTRY(1 oz)',403,7,44.6,1.3,71,1.96,'DESSERT','V').
+food('FRUIT DANISH PASTRY(1 oz)',371,5.4,47.8,1.9,46,1.77,'DESSERT','V').
+food('APPLE PIE(1 oz)',237,1.9,34,1.6,11,1.21,'DESSERT','V').
+food('APPLE STRUDEL(1 oz)',274,3.3,41.1,2.2,15,0.42,'DESSERT','V').
+food('BROWNIE(1 brownie,(2" square))',405,4.57,64.95,0,42,2.15,'DESSERT','V').
+food('CHEESECAKE (1 oz)',274,5.5,35.5,1.9,172,0.47,'DESSERT','V').
+food('CINNAMON BUNS(1 bun)',452,4.45,48.6,1.2,183,1.37,'DESSERT','V').
+food('CREME FILLED DOUGHNUTS(1 oz)',361,6.4,30,0.8,25,1.83,'DESSERT','V').
+food('POUND CAKE(1 oz)',283,5.4,61,1.1,43,2.06,'DESSERT','V').
+food('SPINACH SPAGHETTI(1 cup)',130,4.58,26.15,0,30,1.04,'DMAIN','V').
+food('SPONGE CAKE(1 oz)',290,5.4,61,0.5,70,2.72,'DESSERT','V').
+food('VANILLA PUDDING(1 package)',369,0.3,92.4,0,15,0.05,'DESSERT','V').
+food('SOFT SERVE CHOCOLATE ICECREAM(0.5 cup)',222,4.1,22.2,0.7,131,0.21,'DESSERT','V').
+food('CHOCOLATE ICECREAM BAR(1 bar)',331,4.1,24.5,0.8,119,0.29,'DESSERT','V').
+food('ICECREAM COOKIE SANDWICH(1 serving)',240,3.7,39.6,1.2,73,1.1,'DESSERT','V').
+food('CANDIED FRUIT',322,0.34,82.74,1.6,18,0.17,'DESSERT','V').
+food('BEAN BURRITO(1 each, burrito)',209,7.35,31.23,4.2,124,2.37,'DMAIN','V').
+food('BEEF POT PIE(1 pie, cooked (average weight))',220,7.25,22.05,0.8,14,1.25,'DMAIN2','N').
+food('CHICKEN CHOW MEIN(1 order)',85,6.76,8.29,1,21,0.67,'DMAIN2','N').
+food('HAM AND CHEESE SANDWICH(1 serving)',241,9.16,30.55,1.5,152,2,'DMAIN2','N').
+food('LASAGNA(1 serving)',139,6.87,14.18,1.9,150,0.55,'DMAIN2','V').
+food('MACARONI(1 cup)',149,5.6,17.28,1.1,114,0.57,'DMAIN2','V').
+food('MEATBALLS & MOZZARELLA SANDWICH(1 NLEA serving)',253,9.32,30.45,2.2,181,2.08,'DMAIN2','N').
+food('PASTA WITH EGG(2 oz)',130,5.28,23.54,0,10,1.16,'DMAIN2','N').
+food('PASTA WITHOUT EGG(2 oz)',124,4.37,25.12,0,6,1.13,'DMAIN2','V').
+food('PEPPERONI PIZZA(1 serving)',296,11.21,28.51,2.2,151,2.41,'DMAIN2','N').
+food('PIZZA ROLLS(1 serving,6 rolls)',328,8.73,50.72,1.2,125,1.8,'DMAIN2','V').
+food('QUESADILLA(1 serving,serving size varied on diameter and count)',368,15.93,24.11,1.7,430,1.52,'DMAIN2','V').
+food('RAVIOLI CHEESE-FILLED(1 cup)',77,2.48,13.64,1.3,33,0.74,'DMAIN2','V').
+food('RAVIOLI MEAT FILLED(1 cup)',97,3.24,13.26,1.5,12,1.09,'DMAIN2','N').
+food('RICE NOODLES(1 cup)',108,1.79,24.01,1,4,0.14,'DMAIN2','V').
+food('SPAGHETTI(1 serving)',121,5.79,16.4,1.6,17,0.97,'DMAIN2','V').
+food('SPAGHETTI WITH MEATBALLS(1 cup)',100,4.37,11.45,2.7,29,1.03,'DMAIN2','N').
+food('VEG CHOW MEIN (1 serving)',43,1.34,5.74,1.2,25,0.42,'DMAIN2','V').
+food('ALMONDS(1 cup, whole)',579,21.15,21.55,12.5,269,3.71,'FNN','V').
+food('BLUEBERRIES',61,1.22,12.31,2.6,13,0.37,'FNN','V').
+food('PECANS(1 cup)',715,9.2,13.01,9.5,67,2.47,'FNN','V').
+food('SALTED CASHEW NUTS(1 cup)',574,15.31,32.69,3,45,6,'FNN','V').
+food('SALTED PISTACHIO NUTS(1 cup)',564,20.95,28.66,9.9,107,4.03,'FNN','V').
+food('TRAIL MIX(1 cup)',462,13.8,44.9,0,78,3.05,'FNN','V').
+food('WALNUTS(1 cup)',654,15.23,13.71,6.7,98,2.91,'FNN','V').
+food('APPLES(1 cup, quartered or chopped)',52,0.26,13.81,2.4,6,0.12,'FNN','V').
+food('APRICOTS(1 cup,halves)',48,1.4,11.12,2,13,0.39,'FNN','V').
+food('AVOCADOS(1 cup, cubes)',160,2,8.53,6.7,12,0.55,'FNN','V').
+food('BANANAS(1 cup, mashed)',89,1.09,22.84,2.6,5,0.26,'FNN','V').
+
